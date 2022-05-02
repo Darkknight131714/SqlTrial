51484f45ebf722257f09f2f7fdbb6d613d084b16
Gautam <darkknight131714@gmail.com>
2022-05-02 14:35:02 +0530

    just trying it

--- lib/main.dart
+++ lib/main.dart
@@ -203,7 +203,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: LiquidPullToRefresh(
         showChildOpacityTransition: false,
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.orange,
         onRefresh: trial,
         child: Column(
           mainAxisSize: MainAxisSize.min,
